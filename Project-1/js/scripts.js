dc86401a8975d82c594e57b559e026e9fab76f78
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2023-03-07 20:47:46 -0600

    Completed Project 1

---
+++ Project-1/js/scripts.js
@@ -0,0 +1,13 @@
+var site = 'NBA Info';
+var salutation = 'Welcome to, ';
+var greeting = salutation + site + '! Here is the latest in NBA Rankings.';
+var WebInfoEl = document.getElementById('WebInfo');
+
+WebInfoEl.textContent = greeting;
+
+var numOfRanks = 5;
+var quantity = 'Number of rankings';
+var rank = quantity + 'todays is ' + numOfRanks;
+var rankingsEl = document.getElementById('Rankings');
+
+rankingsEl.textContent = rank;
